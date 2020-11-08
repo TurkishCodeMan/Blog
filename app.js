98a346666b99a9c877c40dc991c3f1086b41c20f
CodeMan <huseyinaltikulac@gmail.com>
2020-11-08 20:50:40 +0300

    Edit Project

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ app.use(morgan("dev"))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(express.static('public')); // for serving the HTML file
+app.use(express.static(__dirname + "/public/"));; // for serving the HTML file
 
 
 //Handle Production
